a59b1fd42eb64340a9e5265c4ebc67495e12e3ee
anthony_ricse <dokouser98@gmail.com>
2021-07-07 16:58:06 -0500

    startInkafarma

--- src/app.js
+++ src/app.js
@@ -8,8 +8,6 @@ app.use(express.json());
 
 app.use(require('./routes/email'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.listen(3000, () => {
     console.log('server on port 3000');
 });
